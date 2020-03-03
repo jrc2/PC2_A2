45aadb00a3ea1abac9501f8415120b9a294643e3
John Chittam <john.chittam@gmail.com>
2020-03-03 13:53:30 -0500

    changed for loop to use i++ instead of ++i

--- main.cpp
+++ main.cpp
@@ -9,11 +9,10 @@ int main(int args_count, char *args[])
 {
     cout << "Num args: " << args_count << endl;
     cout << "Args:" << endl;
-    for (int i = 0; i < args_count; ++i)
+    for (int i = 0; i < args_count; i++)
     {
         cout << args[i] << endl;
     } //for debugging command line args
-
 
     Controller controller;
     controller.run();
