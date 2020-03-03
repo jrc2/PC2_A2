91a8aa836d8597162430c6e5973d2d87bdf9fe34
John Chittam <john.chittam@gmail.com>
2020-03-02 22:54:20 -0500

    added command line args parameters to main function and some print statements for debugging args

--- main.cpp
+++ main.cpp
@@ -5,8 +5,16 @@ using namespace std;
 #include "Controller.h"
 using namespace controller;
 
-int main()
+int main(int args_count, char *args[])
 {
+    cout << "Num args: " << args_count << endl;
+    cout << "Args:" << endl;
+    for (int i = 0; i < args_count; ++i)
+    {
+        cout << args[i] << endl;
+    } //for debugging command line args
+
+
     Controller controller;
     controller.run();
     return 0;
