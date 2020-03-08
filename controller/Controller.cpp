c0ceb888961e65cfe8e6c1f5f5501bc699c58a8f
John Chittam <john.chittam@gmail.com>
2020-03-07 22:44:07 -0500

    added checks for --help to all argument checks

--- controller/Controller.cpp
+++ controller/Controller.cpp
@@ -22,6 +22,7 @@ Controller::~Controller()
 void Controller::run(int args_count, char *args[])
 {
     WordCounter word_counter;
+    const string help = "--help";
     string infile = "";
     string outfile = "";
     map<string, int> words_and_count_to_add;
@@ -35,7 +36,7 @@ void Controller::run(int args_count, char *args[])
     {
         Utilities utilities;
         string arg = args[i];
-        if (arg.compare("--help") == 0)
+        if (arg.compare(help) == 0)
         {
             cout << utilities.get_help_message() << endl; //TODO check if cout help message here is ok
             return;
@@ -44,17 +45,32 @@ void Controller::run(int args_count, char *args[])
         {
             string word = args[++i];
             string num_times = args[++i];
+            if (word.compare(help) == 0 || num_times.compare(help) == 0)
+            {
+                cout << utilities.get_help_message() << endl;
+                return;
+            }
             cout << "adding " << word << " " << num_times << " times" << endl;
         }
         else if (arg.compare("-c") == 0)
         {
             string num_columns = args[++i];
+            if (num_columns.compare(help) == 0)
+            {
+                cout << utilities.get_help_message() << endl;
+                return;
+            }
             cout << "changing num cols to " << num_columns << endl;
         }
         else if (arg.compare("-d") == 0)
         {
             string word = args[++i];
             string num_times = args[++i];
+            if (word.compare(help) == 0 || num_times.compare(help) == 0)
+            {
+                cout << utilities.get_help_message() << endl;
+                return;
+            }
             cout << "deleting " << word << " " << num_times << " times" << endl;
         }
         else if (arg.compare("-oa") == 0)
@@ -64,6 +80,11 @@ void Controller::run(int args_count, char *args[])
         else if (arg.compare("-r") == 0)
         {
             string word_to_remove = args[++i];
+            if (word_to_remove.compare(help) == 0)
+            {
+                cout << utilities.get_help_message() << endl;
+                return;
+            }
             cout << "removing completely " << word_to_remove << endl;
         }
         else if (arg.compare("-o") == 0)
@@ -73,6 +94,11 @@ void Controller::run(int args_count, char *args[])
         else if (arg.compare("-w") == 0)
         {
             string column_width = args[++i];
+            if (column_width.compare(help) == 0)
+            {
+                cout << utilities.get_help_message() << endl;
+                return;
+            }
             cout << "changing col width to " << column_width << endl;
         }
         else if (infile.compare(""))
